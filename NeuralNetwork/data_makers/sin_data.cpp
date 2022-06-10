80b89ce86e315c4ebdbf599bf8cf442377d7b42c
tsubasa-alife <tsubasa.robotics@gmail.com>
2022-06-11 02:28:15 +0900

    データ作成用のスクリプトを別リポジトリから移動

---
+++ NeuralNetwork/data_makers/sin_data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+int main(){
+    double x = 0;
+    std::ofstream fout("sin_data.txt");
+    for(int i = 0; i<20;i++){
+        x = 0.8*sin(M_PI*i/10);
+        fout << x << std::endl;
+    }
+    fout.close();
+}
